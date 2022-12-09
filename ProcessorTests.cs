dadf5677324c9afcd6c797765dabaa225fe0933a
Gyula Kristo <gyula_kristo@trimble.com>
2022-12-09 14:33:45 +0200

    Add project files.

---
+++ ProcessorTests.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using NUnit.Framework;
+using static TestMoq2.Program;
+
+namespace TestMoq2
+{
+   [TestFixture]
+   public class ProcessorTests
+   {
+      private Mock<IService> _serviceMock;
+      private Processor _processor;
+
+      [SetUp]
+      public void Setup()
+      {
+         _serviceMock = new Mock<IService>();
+         _processor = new Processor(_serviceMock.Object);
+      }
+
+      [Test]
+      public void Test()
+      {
+         /*_serviceMock.Setup(service => service.DoSomething(
+            It.Is<InputParameters>(input => input.x == 5)
+         )).Returns(() => true);
+
+         _serviceMock.Setup(service => service.DoSomething(
+            It.Is<InputParameters>(input => input.x == 50)
+         )).Returns(() => false);*/
+
+         _serviceMock.Setup(service => service.DoSomething(
+            It.IsAny<InputParameters>()
+         ));
+
+         _processor.Process();
+
+         _serviceMock.Verify(service => service.DoSomething(
+            It.Is<InputParameters>(input => input.x == 5)
+         ), Times.Exactly(1)); //returns 0 instead of 1
+
+         _serviceMock.Verify(service => service.DoSomething(
+            It.Is<InputParameters>(input => input.x == 100)
+         ), Times.Exactly(1)); //returns 2 instead of 1
+
+         Assert.NotNull(_processor);
+      }
+   }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMoq2
+{
+   public class Program
+   {
+      public class InputParameters
+      {
+         public int x, y;
+      }
+
+      public interface IService
+      {
+         bool DoSomething(InputParameters input);
+      }
+
+      public class Service : IService
+      {
+         public bool DoSomething(InputParameters input)
+         {
+            return input.x > input.y;
+         }
+      }
+
+      public class Processor
+      {
+         public IService _service;
+
+         public Processor(IService theServive)
+         {
+            _service = theServive;
+         }
+
+         public void Process()
+         {
+            InputParameters input = new InputParameters();
+            input.x = 5;
+            input.y = 50;
+
+            _service.DoSomething(input);
+
+            input.x = 100;
+
+            _service.DoSomething(input);
+         }
+      }
+
+      static void Main(string[] args)
+      {
+         Processor processor = new Processor(new Service());
+         processor.Process();
+
+         Console.WriteLine("Done!");
+      }
+   }
+}
